c8e94b8e9c989cc1d5c45760e79595b443722fe0
keydil <fadhilfirdausadha493@gmail.com>
2026-07-27 10:01:04 +0700

    fix: add all resource images to Vite input for production build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/images/image_ee43ecbd.svg',
+                'resources/images/image_ee43ecbd.png',
+                'resources/images/Bhumi_Ageung_Talaga.png',
+                'resources/images/museumtalagamanggung.png',
+                'resources/images/stog.png',
+                'resources/images/webicon.png',
+                'resources/images/facebook.svg',
+                'resources/images/youtube.svg',
+            ],
             refresh: true,
         }),
     ],

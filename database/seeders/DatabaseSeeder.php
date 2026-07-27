72149009abb248d8a957a5ffa3e57229d78bb77f
keydil <fadhilfirdausadha493@gmail.com>
2026-07-27 10:56:46 +0700

    feat(seeder): add default admin credentials to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@museum.com'],
+            [
+                'name' => 'Administrator Museum',
+                'password' => \Illuminate\Support\Facades\Hash::make('admin123'),
+            ]
+        );
     }
 }
